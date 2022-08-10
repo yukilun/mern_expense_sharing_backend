1095b559d8dc82869633b8e6d5601df219bd7784
yukilun <102889935+yukilun@users.noreply.github.com>
2022-08-10 00:08:52 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const expenseRouter = require("./routes/expense_routes");
 const port = process.env.PORT || 5000;
+const apikey = process.env.API_KEY;
 const bodyParser = require('body-parser');
 
 app.use(cors());
 //express.json() is a method inbuilt in express to recognize the incoming Request Object as a JSON Object. 
 app.use(express.json());
 
-const uri = "mongodb+srv://test:test123@cluster0.lenqf.mongodb.net/expenseSharing?retryWrites=true&w=majority";
+const uri = `mongodb+srv://user:${apikey}@cluster0.lenqf.mongodb.net/expenseSharing?retryWrites=true&w=majority`;
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const connection = mongoose.connection;
